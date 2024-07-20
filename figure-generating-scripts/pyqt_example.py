bc67074a34b9b63db878f02420ce60be3628b02b
Marc Lichtman <marcll@vt.edu>
2024-07-19 23:28:06 -0400

    cleanups

--- figure-generating-scripts/pyqt_example.py
+++ figure-generating-scripts/pyqt_example.py
@@ -29,7 +29,7 @@ sdr.rx_hardwaregain_chan0 = gain # dB
 
 class SDRWorker(QObject):
     time_plot_update = pyqtSignal(np.ndarray)
-    fft_plot_update = pyqtSignal(np.ndarray)
+    freq_plot_update = pyqtSignal(np.ndarray)
     waterfall_plot_update = pyqtSignal(np.ndarray)
     end_of_run = pyqtSignal() # happens many times a second
 
@@ -63,7 +63,7 @@ class SDRWorker(QObject):
         
         PSD = 10.0*np.log10(np.abs(np.fft.fftshift(np.fft.fft(samples)))**2/(fft_size*sample_rate))
         self.PSD_avg = self.PSD_avg * 0.99 + PSD * 0.01
-        self.fft_plot_update.emit(self.PSD_avg)
+        self.freq_plot_update.emit(self.PSD_avg)
     
         self.spectrogram[:] = np.roll(self.spectrogram, 1, axis=1) # shifts waterfall 1 row
         self.spectrogram[:,0] = PSD # fill last row with new fft results
@@ -89,34 +89,33 @@ class MainWindow(QMainWindow):
         # Initialize worker and thread
         self.sdr_thread = QThread()
         self.sdr_thread.setObjectName('SDR_Thread') # so we can see it in htop, note you have to hit F2 -> Display options -> Show custom thread names
-        self.worker = SDRWorker()
-        self.worker.moveToThread(self.sdr_thread)
+        worker = SDRWorker()
+        worker.moveToThread(self.sdr_thread)
 
         # Time plot
-        self.time_plot = pg.PlotWidget(labels={'left': 'Amplitude', 'bottom': 'Time [microseconds]'}, enableMenu=False)
-        #self.time_plot.getPlotItem().getViewBox().setMouseMode(pg.ViewBox.RectMode)
-        self.time_plot.setMouseEnabled(x=False, y=True)
-        self.time_plot.setYRange(-1.1, 1.1)
-        self.time_plot_curve_i = self.time_plot.plot([]) 
-        self.time_plot_curve_q = self.time_plot.plot([]) 
-        layout.addWidget(self.time_plot, 1, 0)
+        time_plot = pg.PlotWidget(labels={'left': 'Amplitude', 'bottom': 'Time [microseconds]'}, enableMenu=False)
+        time_plot.setMouseEnabled(x=False, y=True)
+        time_plot.setYRange(-1.1, 1.1)
+        time_plot_curve_i = time_plot.plot([]) 
+        time_plot_curve_q = time_plot.plot([]) 
+        layout.addWidget(time_plot, 1, 0)
 
         # Time plot auto range button
         auto_range_button = QPushButton('Auto Range')
-        auto_range_button.clicked.connect(lambda : self.time_plot.autoRange()) # lambda just means its an unnamed function
+        auto_range_button.clicked.connect(lambda : time_plot.autoRange()) # lambda just means its an unnamed function
         layout.addWidget(auto_range_button, 1, 1)
 
-        # create fft plot
-        self.fft_plot = pg.PlotWidget(labels={'left': 'PSD', 'bottom': 'Frequency [MHz]'}, enableMenu=False)
-        self.fft_plot.setMouseEnabled(x=False, y=True)
-        self.fft_plot_curve_fft = self.fft_plot.plot([]) 
-        self.fft_plot.setXRange(center_freq/1e6 - sample_rate/2e6, center_freq/1e6 + sample_rate/2e6)
-        self.fft_plot.setYRange(-60, -40)
-        layout.addWidget(self.fft_plot, 2, 0)
+        # Freq plot
+        freq_plot = pg.PlotWidget(labels={'left': 'PSD', 'bottom': 'Frequency [MHz]'}, enableMenu=False)
+        freq_plot.setMouseEnabled(x=False, y=True)
+        freq_plot_curve = freq_plot.plot([]) 
+        freq_plot.setXRange(center_freq/1e6 - sample_rate/2e6, center_freq/1e6 + sample_rate/2e6)
+        freq_plot.setYRange(-60, -40)
+        layout.addWidget(freq_plot, 2, 0)
         
-        # FFT auto range button
+        # Freq auto range button
         auto_range_button = QPushButton('Auto Range')
-        auto_range_button.clicked.connect(lambda : self.fft_plot.autoRange()) # lambda just means its an unnamed function
+        auto_range_button.clicked.connect(lambda : freq_plot.autoRange()) # lambda just means its an unnamed function
         layout.addWidget(auto_range_button, 2, 1)
 
         # Layout container for waterfall related stuff
@@ -124,27 +123,26 @@ class MainWindow(QMainWindow):
         layout.addLayout(waterfall_layout, 3, 0)
 
         # Waterfall plot
-        self.waterfall = pg.PlotWidget(labels={'left': 'Time [s]', 'bottom': 'Frequency [MHz]'}, enableMenu=False)
-        #self.waterfall.getPlotItem().getViewBox().translateBy(x=10.0)
-        self.imageitem = pg.ImageItem(axisOrder='col-major') # some of these args are purely for performance
-        self.imageitem.setColorMap(pg.colormap.get('viridis', source='matplotlib'))
-        self.imageitem.setLevels((-60, -40))
-        self.waterfall.addItem(self.imageitem)
-        self.waterfall.setMouseEnabled(x=False, y=False)
-        waterfall_layout.addWidget(self.waterfall)
+        waterfall = pg.PlotWidget(labels={'left': 'Time [s]', 'bottom': 'Frequency [MHz]'}, enableMenu=False)
+        imageitem = pg.ImageItem(axisOrder='col-major') # this arg is purely for performance
+        imageitem.setColorMap(pg.colormap.get('viridis', source='matplotlib'))
+        imageitem.setLevels((-60, -40))
+        waterfall.addItem(imageitem)
+        waterfall.setMouseEnabled(x=False, y=False)
+        waterfall_layout.addWidget(waterfall)
 
         # Colorbar for waterfall
         colorbar = pg.GraphicsLayoutWidget() # the bar needs a widget to be contained in
         colorbar.setMaximumWidth(80)
         bar = pg.ColorBarItem(colorMap=pg.colormap.get('viridis', source='matplotlib'), width=40)
-        bar.setImageItem(self.imageitem)
+        bar.setImageItem(imageitem)
         colorbar.addItem(bar)
         waterfall_layout.addWidget(colorbar)
 
         # Waterfall auto range button
         auto_range_button = QPushButton('Auto Range')
         def update_colormap():
-            self.imageitem.setLevels((self.spectrogram_min + 5, self.spectrogram_max - 5))
+            imageitem.setLevels((self.spectrogram_min + 5, self.spectrogram_max - 5))
             bar.setLevels((self.spectrogram_min + 5, self.spectrogram_max - 5))
         auto_range_button.clicked.connect(update_colormap)
         layout.addWidget(auto_range_button, 3, 1)
@@ -155,11 +153,11 @@ class MainWindow(QMainWindow):
         freq_slider.setValue(int(center_freq/1e3))
         freq_slider.setTickPosition(QSlider.TickPosition.TicksBelow)
         freq_slider.setTickInterval(int(1e6))
-        freq_slider.sliderMoved.connect(self.worker.update_freq) # there's also a valueChanged option
+        freq_slider.sliderMoved.connect(worker.update_freq) # there's also a valueChanged option
         freq_label = QLabel()
         def update_freq_label(val):
             freq_label.setText("Frequency [MHz]: " + str(val/1e3))
-            self.fft_plot.autoRange()
+            freq_plot.autoRange()
         freq_slider.sliderMoved.connect(update_freq_label)
         update_freq_label(freq_slider.value()) # initialize the label
         layout.addWidget(freq_slider, 4, 0)
@@ -171,7 +169,7 @@ class MainWindow(QMainWindow):
         gain_slider.setValue(gain)
         gain_slider.setTickPosition(QSlider.TickPosition.TicksBelow)
         gain_slider.setTickInterval(2)
-        gain_slider.sliderMoved.connect(self.worker.update_gain)
+        gain_slider.sliderMoved.connect(worker.update_gain)
         gain_label = QLabel()
         def update_gain_label(val):
             gain_label.setText("Gain: " + str(val))
@@ -183,7 +181,7 @@ class MainWindow(QMainWindow):
         # Sample rate dropdown (QComboBox)
         sample_rate_combobox = QComboBox()
         sample_rate_combobox.addItems([str(x) + ' MHz' for x in sample_rates])
-        sample_rate_combobox.currentIndexChanged.connect(self.worker.update_sample_rate)
+        sample_rate_combobox.currentIndexChanged.connect(worker.update_sample_rate)
         sample_rate_label = QLabel()
         def update_sample_rate_label(val):
             sample_rate_label.setText("Sample Rate: " + str(sample_rates[val]) + " MHz")
@@ -198,29 +196,29 @@ class MainWindow(QMainWindow):
 
         # Signals and slots stuff
         def time_plot_callback(samples):
-            self.time_plot_curve_i.setData(samples.real)
-            self.time_plot_curve_q.setData(samples.imag)
+            time_plot_curve_i.setData(samples.real)
+            time_plot_curve_q.setData(samples.imag)
         
-        def fft_plot_callback(PSD_avg):
+        def freq_plot_callback(PSD_avg):
             # TODO figure out if there's a way to just change the visual ticks instead of the actual x vals
             f = np.linspace(freq_slider.value()*1e3 - sample_rate/2.0, freq_slider.value()*1e3 + sample_rate/2.0, fft_size) / 1e6
-            self.fft_plot_curve_fft.setData(f, PSD_avg)
-            self.fft_plot.setXRange(freq_slider.value()*1e3/1e6 - sample_rate/2e6, freq_slider.value()*1e3/1e6 + sample_rate/2e6)
+            freq_plot_curve.setData(f, PSD_avg)
+            freq_plot.setXRange(freq_slider.value()*1e3/1e6 - sample_rate/2e6, freq_slider.value()*1e3/1e6 + sample_rate/2e6)
         
         def waterfall_plot_callback(spectrogram):
-            self.imageitem.setImage(spectrogram, autoLevels=False) 
+            imageitem.setImage(spectrogram, autoLevels=False) 
             self.spectrogram_min = np.min(spectrogram) # save the min and max in window state
             self.spectrogram_max = np.max(spectrogram)
 
         def end_of_run_callback():
-            QTimer.singleShot(0, self.worker.run) # Run worker again immediately
+            QTimer.singleShot(0, worker.run) # Run worker again immediately
         
-        self.worker.time_plot_update.connect(time_plot_callback) # connect the signal to the callback
-        self.worker.fft_plot_update.connect(fft_plot_callback)
-        self.worker.waterfall_plot_update.connect(waterfall_plot_callback)
-        self.worker.end_of_run.connect(end_of_run_callback)
+        worker.time_plot_update.connect(time_plot_callback) # connect the signal to the callback
+        worker.freq_plot_update.connect(freq_plot_callback)
+        worker.waterfall_plot_update.connect(waterfall_plot_callback)
+        worker.end_of_run.connect(end_of_run_callback)
 
-        self.sdr_thread.started.connect(self.worker.run) # kicks off the worker when the thread starts
+        self.sdr_thread.started.connect(worker.run) # kicks off the worker when the thread starts
         self.sdr_thread.start()
 
 

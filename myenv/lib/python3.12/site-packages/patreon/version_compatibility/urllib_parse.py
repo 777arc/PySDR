2eac79ca20921dc09feedbddc5baa34fa6cc95fc
Suhail Ahmed <suhailkvaliganj@gmail.com>
2025-06-14 17:27:26 +0530

    Remove myenv from repo and add to .gitignore

--- myenv/lib/python3.12/site-packages/patreon/version_compatibility/urllib_parse.py
+++
@@ -1,7 +0,0 @@
-try:
-    # python2
-    from urllib import urlencode
-    from urlparse import urlparse, parse_qs
-except ImportError:
-    # python3
-    from urllib.parse import urlencode, urlparse, parse_qs
